3075d8c9d1e409484ab87f7b7d7a1aebc8aa3bc2
Andrei Pavlovich <pap_27.97@mail.ru>
2018-11-30 22:10:53 +0300

    Create view.py

---
+++ view/view.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 30 21:16:01 2018
+
+@author: User
+"""
+
+import tkinter as tk
+import tkinter.ttk as ttk
+
+class Window(ttk.Frame):
+    def __init__(slef,master=None):
+        super().__init__(master)
+        self.creat_varibles()
+        self.creat_ui()
+    
+    def creat_varibles(self):
+        pass
+    
+    def creat_ui(self):
+        pass
+    
