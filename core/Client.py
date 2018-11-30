368de2f453ab00e2198ae7e5487fd8053f503f89
Andrei Pavlovich <pap_27.97@mail.ru>
2018-11-30 05:13:48 +0300

    Update Client.py
    added input/output buffer function

--- core/Client.py
+++ core/Client.py
@@ -1,26 +1,41 @@
 # -*- coding: utf-8 -*-
 import socket
 import threading
-
+from collections import deque
 class Client:
     
-    def __init__(self):
-        self.__host
-        self.__port
-        self.__socket
-        self.__input_buffer
-        self.__output_buffer
+    def __init__(self, host,port):
+        self.__host = host
+        self.__port = port
+        self.__socket = socket(AF_INET, SOCK_STREAM)
+        self.__input_buffer = deque()
+        self.__output_buffer = deque()
     
     def run(self):
         pass        
-    def sendMsg(self):
+    def __sendMsg(self):
         pass
-    def receivMsg(self):
+    def __receivMsg(self):
         pass
-    def get_input_buffer(self):
-        pass
-    def get_output_buffer(self):
-        pass
+    
+    def __input_buffer_appand(self, msg):
+        self.__input_buffer.append(msg)   
+    
+    def __output_buffer_appand(self, msg):
+        self.__output_buffer.append(msg)   
+        
+    def __get_input_buffer(self):
+        try:
+            return self.__input_buffer.popleft()
+        except IndexError:
+            return None
+
+    def __get_output_buffer(self):
+        try:
+            return self.__output_buffer.popleft()
+        except IndexError:
+            return None
+
     def get_port(self):
         pass    
     def get_host(self):
