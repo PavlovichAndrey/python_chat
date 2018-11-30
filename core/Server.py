a3a8406a5c5923c2c75ebb1086bfac1b720f5bf6
Andrei Pavlovich <pap_27.97@mail.ru>
2018-11-30 03:45:34 +0300

    Create Server.py
    creat base of Server class

---
+++ core/Server.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import socket
+import threading
+
+class Client:
+    
+    def __init__(self):
+        self.__host
+        self.__port
+        self.__socket
+        self.__clients
+        
+    def run(self):
+        
+    def connect_client(self):
+        
+    def start_communication(self):
+            
+    def broadcust(self):
+    
+    def get_port(self):
+        
+    def get_host(self):
+    
+
+
