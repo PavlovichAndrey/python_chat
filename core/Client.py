155e9fc62b7fcb222f7b181f202221ab0731e4e7
Andrei Pavlovich <pap_27.97@mail.ru>
2018-11-30 06:02:16 +0300

    Update Client.py

--- core/Client.py
+++ core/Client.py
@@ -4,22 +4,28 @@ import threading
 from collections import deque
 class Client:
     
-    def __init__(self, host,port):
+    def __init__(self, host,port, name):
         self.__host = host
         self.__port = port
+        self.__name = name
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__input_buffer = deque()
         self.__output_buffer = deque()
+        self.__output_buffer_appand(name)
     
     def run(self):
+        try:
+            self.__socket.connect((self.get_host,self.get_port))
+        except:
+            pass
         threading.Thread(target = self.__receiv_msg).start()
-        self.send_msg()        
+        self.__send_msg()        
     
     def __send_msg(self):
         while True: 
             msg = self.__get_output_buffer()
             if msg is not None:
-                self.__socket.send(msg)        
+                self.__socket.send(str.encode(msg))        
 
     def __receiv_msg(self):
         while True:
@@ -32,9 +38,11 @@ class Client:
     
     def __input_buffer_appand(self, msg):
         self.__input_buffer.append(msg)   
+        self.log(" i_b_a: "+ str(msg))
     
     def __output_buffer_appand(self, msg):
         self.__output_buffer.append(msg)   
+        self.log(" o_b_a: " + str(msg))
         
     def __get_input_buffer(self):
         try:
@@ -54,3 +62,9 @@ class Client:
         pass
     def log(self, msg):
         print("Client.log: ",msg)
+
+c = Client("",777,"andrei")
+c.get_host
+c.get_port
+c.log('test')
+c.run()
