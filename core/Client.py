018bdee9a3c4b6625714265382590ad4b176fd87
Andrei Pavlovich <pap_27.97@mail.ru>
2018-11-30 04:06:18 +0300

    Update Client.py

--- core/Client.py
+++ core/Client.py
@@ -12,17 +12,18 @@ class Client:
         self.__output_buffer
     
     def run(self):
-        
+        pass        
     def sendMsg(self):
-        
+        pass
     def receivMsg(self):
-        
+        pass
     def get_input_buffer(self):
-    
+        pass
     def get_output_buffer(self):
-    
+        pass
     def get_port(self):
-        
+        pass    
     def get_host(self):
-    
-
+        pass
+    def log(self, msg):
+        print(msg)
