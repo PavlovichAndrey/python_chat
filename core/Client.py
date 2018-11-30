ef32c5c1e6e016d956585de9dafe9b862d6c2180
Andrei Pavlovich <pap_27.97@mail.ru>
2018-11-30 03:40:09 +0300

    Create Client.py
    creat base of client class

---
+++ core/Client.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import socket
+import threading
+
+class Client:
+    
+    def __init__(self):
+        self.__host
+        self.__port
+        self.__socket
+        self.__input_buffer
+        self.__output_buffer
+    
+    def run(self):
+        
+    def sendMsg(self):
+        
+    def receivMsg(self):
+        
+    def get_input_buffer(self):
+    
+    def get_output_buffer(self):
+    
+    def get_port(self):
+        
+    def get_host(self):
+    
+
